1457af3e82b7bd826c300eab15b97bff88b57ec7
Sopianhidayat2014 <sopianhidayat62@gmail.com>
2020-12-09 10:13:06 +0700

    mencariperbedaan2waktu

--- mencariperbedaan2waktu.cpp
+++ mencariperbedaan2waktu.cpp
@@ -5,9 +5,12 @@ int main()
 {
 	int j1,m1,d1,td1;
 	int j2,m2,d2,td2;
-	int j3,m3,d3,td3;
-	int sm;
-	cout<<"===================================="<<endl;
+	int j3,m3,d3,td3,sm3;
+	cout<<"-----------------------"<<endl;
+	cout<<"-PROGRAM WAKTU BILLING-"<<endl;
+	cout<<"-----------------------"<<endl;
+	
+	cout<<"===Waktu Awal==="<<endl;
 	//masukan waktu awal//
 	cout<<"Masukan Jam :";
 	cin>>j1;
@@ -15,30 +18,30 @@ int main()
 	cin>>m1;
 	cout<<"Masukan Detik :";
 	cin>>d1;
-	cout<<"===================================="<<endl;
-	cout<<"===================================="<<endl;
-		//masukan waktu akhir//
+	cout<<"===Waktu Akhir==="<<endl;
+	//masukan waktu akhir//
 	cout<<"Masukan Jam :";
 	cin>>j2;
 	cout<<"Masukan Menit :";
 	cin>>m2;
 	cout<<"Masukan Detik :";
 	cin>>d2;
-	cout<<"===================================="<<endl;
+	cout<<"================="<<endl;
 	//proses konversi waktu//
 	td1=(j1*3600)+(m1*60)+d1;
 	td2=(j2*3600)+(m2*60)+d2;
-	cout<<"===================================="<<endl;
+
 	//operasi pengurangan//
-	td3=td1-td2;
-	cout<<"===================================="<<endl;
+	td3=td2-td1;
+
 	//konversi total detik ke j3,m3,d3//
 	j3=td3/3600;
-	sm=td3%3600;
-	m3=sm/60;
-	d3=sm%60;
-	cout<<"===================================="<<endl;
-	cout<<"Lama Bermain ="<<j3<<":"<<m3<<":"<<d3<<endl;
+	sm3=td3%3600;
+	m3=sm3/60;
+	d3=sm3%60;
+	cout<<"================="<<endl;
+	cout<<"Waktu Yang Digunakan ";
+	cout<<j3<<":"<<m3<<":"<<d3;
 	
 	return 0;
 }
